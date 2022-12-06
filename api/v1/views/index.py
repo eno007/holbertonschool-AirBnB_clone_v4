4e21bd9a29b6a3596dac3c57e864076919056539
FatjonMileti <tonimileti@yahoo.com>
2022-12-06 02:40:25 -0800

    ...

---
+++ api/v1/views/index.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+''' *** *** '''
+from api.v1.views import app_views
+from flask import Flask, jsonify
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status():
+    """ Status of API """
+    return jsonify({"status": "OK"})
