81d0706a422ccef4b1eebdbfcac65d6e7a78a11c
FatjonMileti <tonimileti@yahoo.com>
2022-12-07 08:52:00 -0800

    ...

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -19,6 +19,7 @@ def get_cities(state_id):
         cities.append(city.to_dict())
     return jsonify(cities)
 
+
 @app_views.route('/cities/<string:city_id>', methods=['GET'],
                  strict_slashes=False)
 def get_city(city_id):
@@ -27,6 +28,7 @@ def get_city(city_id):
     if city is None:
         abort(404)
     return jsonify(city.to_dict())
+
 
 @app_views.route('/cities/<string:city_id>', methods=['DELETE'],
                  strict_slashes=False)
@@ -38,6 +40,7 @@ def delete_city(city_id):
     city.delete()
     storage.save()
     return (jsonify({}))
+
 
 @app_views.route('/states/<string:state_id>/cities/', methods=['POST'],
                  strict_slashes=False)
@@ -55,6 +58,7 @@ def post_city(state_id):
     city = City(**kwargs)
     city.save()
     return make_response(jsonify(city.to_dict()), 201)
+
 
 @app_views.route('/cities/<string:city_id>', methods=['PUT'],
                  strict_slashes=False)
